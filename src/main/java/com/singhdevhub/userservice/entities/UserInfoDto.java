2ec2c80d88bb7c3f9b9ca39f479bb8724bb006b0
singhdevhub-lovepreet <lpskshorts@gmail.com>
2024-04-01 22:40:53 +0530

    Fixing kafka deserialisation

--- src/main/java/com/singhdevhub/userservice/entities/UserInfoDto.java
+++ src/main/java/com/singhdevhub/userservice/entities/UserInfoDto.java
@@ -1,16 +1,20 @@
 package com.singhdevhub.userservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
@@ -18,19 +22,25 @@ import lombok.NoArgsConstructor;
 public class UserInfoDto
 {
 
+
     private Long id;
 
     @Id
+    @JsonProperty("user_id")
     private String userId;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
+    @JsonProperty("phone_number")
     private Long phoneNumber;
 
     private String email;
 
+    @JsonProperty("profile_pic")
     private String profilePic;
 
 }
--- src/main/java/com/singhdevhub/userservice/repository/UserRepository.java
+++ src/main/java/com/singhdevhub/userservice/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.singhdevhub.userservice.repository;
 
 import com.singhdevhub.userservice.entities.UserInfoDto;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories
 public interface UserRepository extends CrudRepository<UserInfoDto, String>
 {
 
